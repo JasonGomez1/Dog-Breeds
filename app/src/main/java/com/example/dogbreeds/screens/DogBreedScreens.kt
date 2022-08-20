e6b78db020fda056b89f3e277b570049b0a87160
Jason Gomez <jgomez@weedmaps.com>
2022-08-20 15:12:50 -0700

    Added padding to BreedCard

--- app/src/main/java/com/example/dogbreeds/screens/DogBreedScreens.kt
+++ app/src/main/java/com/example/dogbreeds/screens/DogBreedScreens.kt
@@ -9,10 +9,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -102,12 +98,24 @@ private fun DogBreedsTabBar(
 
 @Composable
 private fun BreedCard(modifier: Modifier = Modifier, breedInfo: BreedInfo) {
-    Card(modifier = modifier.padding(8.dp)) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Text(breedInfo.name, style = MaterialTheme.typography.h6)
-            Row(modifier = Modifier
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 4.dp)) {
+                .padding(8.dp)
+        ) {
+            Text(
+                text = breedInfo.name,
+                style = MaterialTheme.typography.h6
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 4.dp)
+            ) {
                 Image(
                     modifier = Modifier.size(200.dp),
                     painter = painterResource(breedInfo.image),
