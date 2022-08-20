f51bb93894baa6fc158775c314fd516d3944c2a4
Jason Gomez <jgomez@weedmaps.com>
2022-08-20 15:05:12 -0700

    Added BreedCard

--- app/src/main/java/com/example/dogbreeds/screens/DogBreedScreens.kt
+++ app/src/main/java/com/example/dogbreeds/screens/DogBreedScreens.kt
@@ -1,12 +1,22 @@
 package com.example.dogbreeds.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dogbreeds.R
 
 @Composable
 fun DogBreedsScreen() {
@@ -15,6 +25,7 @@ fun DogBreedsScreen() {
         modifier = Modifier.fillMaxSize(),
         selectedTabIndex = selectedTabIndex,
         tabInfos = getTabInfos(),
+        breedInfos = getBreedInfos(),
         onTabClicked = { selectedTabIndex = it }
     )
 }
@@ -26,6 +37,7 @@ private fun DogBreedsScreenPreview() {
         modifier = Modifier.fillMaxSize(),
         selectedTabIndex = 0,
         tabInfos = getTabInfos(),
+        breedInfos = getBreedInfos(),
         onTabClicked = { }
     )
 }
@@ -35,6 +47,7 @@ private fun DogBreedsScreenContent(
     modifier: Modifier = Modifier,
     selectedTabIndex: Int,
     tabInfos: List<TabInfo>,
+    breedInfos: List<BreedInfo>,
     onTabClicked: (Int) -> Unit
 ) {
     Scaffold(
@@ -47,7 +60,14 @@ private fun DogBreedsScreenContent(
             )
         }
     ) {
-
+        LazyColumn {
+            items(breedInfos) {
+                BreedCard(
+                    modifier = Modifier.fillMaxSize(),
+                    breedInfo = it
+                )
+            }
+        }
     }
 }
 
@@ -80,9 +100,39 @@ private fun DogBreedsTabBar(
     }
 }
 
+@Composable
+private fun BreedCard(modifier: Modifier = Modifier, breedInfo: BreedInfo) {
+    Card(modifier = modifier.padding(8.dp)) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(breedInfo.name, style = MaterialTheme.typography.h6)
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 4.dp)) {
+                Image(
+                    modifier = Modifier.size(200.dp),
+                    painter = painterResource(breedInfo.image),
+                    alignment = Alignment.TopStart,
+                    contentDescription = null
+                )
+                Text(
+                    modifier = Modifier.padding(start = 4.dp),
+                    text = breedInfo.description,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
 private data class TabInfo(
     val title: String,
     val index: Int
+)
+
+private data class BreedInfo(
+    val name: String,
+    @DrawableRes val image: Int,
+    val description: String
 )
 
 private fun getTabInfos(): List<TabInfo> =
@@ -94,4 +144,43 @@ private fun getTabInfos(): List<TabInfo> =
         TabInfo("Poodle", 4),
         TabInfo("Siberian Husky", 5),
         TabInfo("French Bulldog", 6)
+    )
+
+private fun getBreedInfos(): List<BreedInfo> =
+    listOf(
+        BreedInfo(
+            name = "Bulldog",
+            image = R.drawable.bulldog,
+            description = "It's a bulldog"
+        ),
+        BreedInfo(
+            name = "German Shepherd",
+            image = R.drawable.german_shepherd,
+            description = "It's a German Shepherd"
+        ),
+        BreedInfo(
+            name = "Labrador Retriever",
+            image = R.drawable.labrador,
+            description = "It's a Labrador Retriever"
+        ),
+        BreedInfo(
+            name = "Golden Retriever",
+            image = R.drawable.golden_retriever,
+            description = "It's a Golden Retriever"
+        ),
+        BreedInfo(
+            name = "Poodle",
+            image = R.drawable.poodle,
+            description = "It's a Poodle"
+        ),
+        BreedInfo(
+            name = "Siberian Husky",
+            image = R.drawable.siberian_husky,
+            description = "It's a Siberian Husky"
+        ),
+        BreedInfo(
+            name = "French Bulldog",
+            image = R.drawable.french_bulldog,
+            description = "It's a French Bulldog"
+        )
     )
